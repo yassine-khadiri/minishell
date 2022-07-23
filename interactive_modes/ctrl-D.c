4b58ae344472c09a808e93474fffedaac2685df6
Yassine Khadiri <ykhadiri@e3r8p5.1337.ma>
2022-07-23 18:30:13 +0100

    minishell

--- interactive_modes/ctrl-D.c
+++ interactive_modes/ctrl-D.c
@@ -6,17 +6,23 @@
 /*   By: ykhadiri <ykhadiri@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/09 13:43:42 by ykhadiri          #+#    #+#             */
-/*   Updated: 2022/07/14 16:19:55 by ykhadiri         ###   ########.fr       */
+/*   Updated: 2022/07/23 18:28:22 by ykhadiri         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../minishell.h"
 
+// printf("\033[XA"); // Move up X lines;
+// printf("\033[XB"); // Move down X lines;
+// printf("\033[XC"); // Move right X column;
+// printf("\033[XD"); // Move left X column;
+// printf("\033[2J"); // Clear screen
+
 void	ft_ctl_d(t_data *cmds)
 {
 	if (!cmds->command_buf)
 	{
-		write(1, "exit\n", 6);
+		printf("\033[11C\033[1Aexit\n");
 		exit(0);
 	}
 }
