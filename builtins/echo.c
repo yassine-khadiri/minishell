3a7c461a3320af57b50b57e4458efcc342cdfe53
Yassine Khadiri <ykhadiri@e3r1p15.1337.ma>
2022-08-06 18:08:42 +0100

    minishell

--- builtins/echo.c
+++ builtins/echo.c
@@ -6,7 +6,7 @@
 /*   By: ykhadiri <ykhadiri@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/09 12:28:52 by ykhadiri          #+#    #+#             */
-/*   Updated: 2022/08/05 16:18:19 by ykhadiri         ###   ########.fr       */
+/*   Updated: 2022/08/06 18:07:07 by ykhadiri         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,12 +16,11 @@ int	check_dash_n(t_data *data, t_command *cmd, int i)
 {
 	int	j;
 
-	(void)data;
 	while (cmd->cmd_array[i])
 	{
 		if (!ft_strcmp(cmd->cmd_array[i], "-n"))
 			i++;
-		if (cmd->cmd_array[i][0] == '-' && cmd->cmd_array[i][1] == 'n')
+		if (cmd->cmd_array[i] && cmd->cmd_array[i][0] == '-' && cmd->cmd_array[i][1] == 'n')
 		{
 			j = 0;
 			while (cmd->cmd_array[i][++j] == 'n');
@@ -53,7 +52,10 @@ int	ft_echo(t_data *data, t_command *cmd, int index)
 	index = check_dash_n(data, cmd, index);
 	while (cmd->cmd_array[index])
 	{
-		printf("%s ", cmd->cmd_array[index]);
+		if (cmd->cmd_array[index + 1])
+			printf("%s ", cmd->cmd_array[index]);
+		else
+			printf("%s", cmd->cmd_array[index]);
 		index++;
 	}
 	printf("%c", data->mode);
