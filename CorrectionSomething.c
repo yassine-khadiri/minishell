115346abde5928ae9f30c6269fbc7ffb48c1cdb3
Hamza Bouqssim <hamzaboukssim6@gmail.com>
2022-08-06 03:33:13 +0100

    there is a Segfault

--- CorrectionSomething.c
+++ CorrectionSomething.c
@@ -7,4 +7,5 @@
 // Wach makhdmnach bhadi ? create_env_list ???? rani 7aydtha !!!
 // khasna ngado Herdocs & Leaks, 3ad bach nchofo t9abi sghar li b9aw
 // exit 100 , echo $? khas t2afichi 100
+// echo -n => segfault
 
